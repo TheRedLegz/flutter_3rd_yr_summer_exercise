6ee1063d2e0c0a69362821c0d5715d559f00202d
augustorasmil <augustorasmil@gmail.com>
2021-07-09 16:13:44 +0800

    test

--- lib/screens/authentication/auth.dart
+++ lib/screens/authentication/auth.dart
@@ -8,69 +8,70 @@ import 'package:get/get.dart';
 
 class AuthenticationScreen extends StatelessWidget {
   final AppController _appController = Get.find();
-
+//test
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Obx(() => SingleChildScrollView(
-        child: Stack(
-          children: [
-            Image.asset(
-              bg3,
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height,
-              fit: BoxFit.cover,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+        backgroundColor: Colors.white,
+        body: Obx(
+          () => SingleChildScrollView(
+            child: Stack(
               children: [
-                SizedBox(height: MediaQuery.of(context).size.width / 1.3),
-                SizedBox(
-                  height: MediaQuery.of(context).size.width / 70,
+                Image.asset(
+                  bg3,
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height,
+                  fit: BoxFit.cover,
                 ),
-                Visibility(
-                    visible: _appController.isLoginWidgetDisplayed.value,
-                    child: LoginWidget()),
-                Visibility(
-                    visible: !_appController.isLoginWidgetDisplayed.value,
-                    child: RegistrationWidget()),
-                SizedBox(
-                  height: 10,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: MediaQuery.of(context).size.width / 1.3),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.width / 70,
+                    ),
+                    Visibility(
+                        visible: _appController.isLoginWidgetDisplayed.value,
+                        child: LoginWidget()),
+                    Visibility(
+                        visible: !_appController.isLoginWidgetDisplayed.value,
+                        child: RegistrationWidget()),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Visibility(
+                      visible: _appController.isLoginWidgetDisplayed.value,
+                      child: BottomTextWidget(
+                        onTap: () {
+                          _appController.changeDIsplayedAuthWidget();
+                        },
+                        text1: "Don\'t have an account?",
+                        text2: "Create account!",
+                      ),
+                    ),
+                    Visibility(
+                      visible: !_appController.isLoginWidgetDisplayed.value,
+                      child: BottomTextWidget(
+                        onTap: () {
+                          _appController.changeDIsplayedAuthWidget();
+                        },
+                        text1: "Already have an account?",
+                        text2: "Sign in!!",
+                      ),
+                    ),
+                  ],
                 ),
-                Visibility(
-                  visible: _appController.isLoginWidgetDisplayed.value,
-                  child: BottomTextWidget(
-                    onTap: () {
-                      _appController.changeDIsplayedAuthWidget();
-                    },
-                    text1: "Don\'t have an account?",
-                    text2: "Create account!",
+                Positioned(
+                  top: MediaQuery.of(context).size.height / 6,
+                  left: 20,
+                  child: Image.asset(
+                    logo2,
+                    width: 140,
                   ),
-                ),
-                Visibility(
-                  visible: !_appController.isLoginWidgetDisplayed.value,
-                  child: BottomTextWidget(
-                    onTap: () {
-                      _appController.changeDIsplayedAuthWidget();
-                    },
-                    text1: "Already have an account?",
-                    text2: "Sign in!!",
-                  ),
-                ),
+                )
               ],
             ),
-            Positioned(
-              top: MediaQuery.of(context).size.height / 6,
-              left: 20,
-              child: Image.asset(
-                logo2,
-                width: 140,
-              ),
-            )
-          ],
-        ),
-      ),)
-    );
+          ),
+        ));
   }
 }
